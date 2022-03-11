c75a7be0d3a26ec5253c17479d4e9686bd896695
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-03-11 19:55:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
+  
+    float a=0,b=0,c=0;
 
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
+    cout<< (a*b)/2 << endl;
+    cout<< a*a << endl;
+    cout<< a*b << endl;
+    cout<< ((a+b)*c)/2 <<endl;
+
 
 
 }
